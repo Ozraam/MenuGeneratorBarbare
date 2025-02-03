e5b5c7aedc6d1dfe728b39d721cb56e5979d7f2d
Ozraam <lhgrillepain@gmail.com>
2025-02-03 19:59:19 +0100

    finished the vertical image generator

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def next_week():
 ## Create a new image 1080*1920 with background of color #FFF4EA
 ## Add two strip of alternate color #E6A515 and #B77236, each of 743px height and 360px wide, the strips start at the bottom and a white space is left at the top
 ## Add the text of the week in each strip
-def setup_img():
+def setup_img_vertical(header):
     img = Image.new('RGBA', (1080, 1920), color = '#FFF4EA')
 
     for i in range(1920 - 743*2, 1920, 743):
@@ -55,8 +55,7 @@ def setup_img():
     font_day = font.font_variant(size=70)
 
     # add day of the week in each strip
-    days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"]
-    for i in range(5):
+    for (i, head) in enumerate(header):
         y = 1920 - 743*2 + 743 * (0 if i < 3 else 1)
         x = 360*i % 1080
 
@@ -67,7 +66,7 @@ def setup_img():
 
         draw.multiline_text(
             (x + 180, y + 50),
-            days[i].upper(), 
+            head.upper(), 
             font=font_day, 
             fill="#FFF4EA", 
             align="center", 
@@ -78,19 +77,92 @@ def setup_img():
 
     return img
 
-def add_meal(img, meal):
-    pass
+
+def resolve_img_path(img_name):
+    return "./Sandwichlogo/" + img_name + ".png"
+
+
+def add_content_vertical(img, week):
+    warning = []
+
+    desired_img_width = 250
+
+    for i, day in enumerate(week):
+        box_y = 1920 - 743*2 + 743 * (0 if i < 3 else 1)
+        box_x = 360*i % 1080
+
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("./OpenSans-VariableFont_wdth,wght.ttf", 45)
+
+        content_y = box_y + 100
+        img_x = box_x + (360 - desired_img_width) // 2
+
+        if (len(day['content']) > 2):
+            day['content'] = day['content'][:2]
+            warning.append("Warning: too many content for a day, only the first two will be displayed, day: " + day["day"])
+
+        for content in day['content']:
+            if (content['is_meal']):
+                sandwichimg = Image.open(resolve_img_path(content['text']))
+                sandwichimg = sandwichimg.convert("RGBA")
+                sandwichimg_size = sandwichimg.size
+                desired_height = desired_img_width * sandwichimg_size[1] // sandwichimg_size[0]
+                sandwichimg = sandwichimg.resize((desired_img_width, desired_height))
+                # print(sandwichimg.mode)
+                img.paste(sandwichimg, (img_x, content_y), sandwichimg)
+
+                name = break_line(tansform_PascalCase_to_string_with_space(content['text']), 300, draw, font).capitalize()
+
+                text_height = draw.multiline_textbbox((0, 0), name, font=font)[3]
+
+                text_offset = text_height // 4
+
+                draw.text((box_x + 180, content_y + desired_height + text_offset), name, font=font, fill="#FFF4EA", align="center", stroke_width=1, stroke_fill="#FFF4EA", anchor="mm")
+            
+            else:
+                text = break_line(content['text'], 350, draw, font)
+                draw.multiline_text(
+                    (box_x + 180, content_y + 643 //4), 
+                    text, font=font, fill="#FFF4EA", align="center", stroke_width=1, stroke_fill="#FFF4EA", anchor="mm")
+    
+            content_y += 50 + desired_height + text_height // 8
+
+    return (img, warning)
+
+def break_line(string, max_width, draw, font):
+    words = string.split(" ")
+    lines = []
+    current_line = ""
+    for word in words:
+        current_width = draw.multiline_textbbox((0, 0), current_line + word, font=font)[2]
+        if current_width > max_width:
+            lines.append(current_line)
+            current_line = ""
+        current_line += word + " "
+    lines.append(current_line)
+    return "\n".join(lines)
+
+## Transform a string in PascalCase to a string with space between each word
+## Example: "PascalCase" -> "Pascal Case"
+def tansform_PascalCase_to_string_with_space(string):
+    final = ""
+    for i in range(len(string)):
+        if string[i].isupper() and i != 0:
+            final += " "
+        final += string[i]
+    return final
 
 def main():
-    img = setup_img()
 
-    with open("meal.json") as f:
-        meal = json.load(f)
+    with open("meal.json", encoding="utf8") as f:
+        week = json.load(f)
+    
+    img = setup_img_vertical(week["header"])
 
-
-
-    img = add_meal(img, meal)
-    img.show()
+    img = add_content_vertical(img, week["content"])
+    if len(img[1]) > 0:
+        print("\n".join(img[1]))
+    img[0].show()
 
 if __name__ == "__main__":
     main()
